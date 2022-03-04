c3b3fc20fef9b082d981b1af4e935ab35a89be47
PrastowoAdi <sinaadi75@gmail.com>
2022-03-04 17:47:58 +0700

    Refactor detail transaction page

---
+++ components/organism/TransactionDetailContent/Row.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/require-default-props */
+interface RowProps {
+    label: string;
+    value: string;
+    className?:string;
+}
+export default function Row(props: RowProps) {
+  const { label, value, className } = props;
+  return (
+    <p className="text-lg color-palette-1 mb-20">
+      {label}
+      {" "}
+      <span
+        className={`purchase-details ${className}`}
+      >
+        {value}
+      </span>
+    </p>
+  );
+}
