9482df22be71822a3bda2ccc46bf1e6ca2c74177
PrastowoAdi <sinaadi75@gmail.com>
2022-03-11 17:28:43 +0700

    Integration vocher detail page with call API

--- services/player.ts
+++ services/player.ts
@@ -1,16 +1,22 @@
 import axios from "axios";
 
+const ROOT_API = process.env.NEXT_PUBLIC_API;
+const API_VERSION = "api/v1";
+
 export async function getFeaturedGame() {
-  const ROOT_API = process.env.NEXT_PUBLIC_API;
-  const API_VERSIOIN = "api/v1";
   const URL = "players/landingpage";
 
-  const response = await axios.get(`${ROOT_API}/${API_VERSIOIN}/${URL}`);
+  const response = await axios.get(`${ROOT_API}/${API_VERSION}/${URL}`);
   const axiosReponse = response.data;
 
   return axiosReponse.data;
 }
 
-export async function getDetailVoucher() {
-  return null;
+export async function getDetailVoucher(id) {
+  const URL = `players/${id}/detail`;
+
+  const response = await axios.get(`${ROOT_API}/${API_VERSION}/${URL}`);
+  const axiosReponse = response.data;
+
+  return axiosReponse.data;
 }
