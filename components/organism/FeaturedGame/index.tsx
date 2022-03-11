64f304e2e27e676e992b726b983be4663284bef5
PrastowoAdi <sinaadi75@gmail.com>
2022-03-11 15:13:39 +0700

    Sample integration API with NextJS

--- components/organism/FeaturedGame/index.tsx
+++ components/organism/FeaturedGame/index.tsx
@@ -1,9 +1,20 @@
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import GameItem from "../../molecules/GameItem";
 
 export default function FeaturedGame() {
+  const [gameList, setGameList] = useState([]);
+
+  useEffect(async() => {
+    const response = await axios.get("https://mernstack-storegg.herokuapp.com/api/v1/players/landingpage");
+    console.log("data", response.data);
+    setGameList(response.data.data);
+  }, []);
+
   return (
     <section className="featured-game pt-50 pb-50">
       <div className="container-fluid">
@@ -17,11 +28,11 @@ export default function FeaturedGame() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          <GameItem title="Super Mechs" category="Mobile" thumbnail="/img/Thumbnail-1.png" />
-          <GameItem title="Call of Duty: Modern" category="Mobile" thumbnail="/img/Thumbnail-2.png" />
-          <GameItem title="Mobile Legends" category="Mobile" thumbnail="/img/Thumbnail-3.png" />
-          <GameItem title="Clash of Clans" category="Mobile" thumbnail="/img/Thumbnail-4.png" />
-          <GameItem title="Valorant" category="Dekstop" thumbnail="/img/Thumbnail-5.png" />
+          {
+            gameList.map((item) => (
+              <GameItem key={item._id} title={item.name} category={item.category.name} thumbnail={`https://mernstack-storegg.herokuapp.com/uploads/${item.thumbnail}`} />
+            ))
+          }
         </div>
       </div>
     </section>
