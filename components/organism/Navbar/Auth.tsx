88f65e25a2f9bfa1083ec6bf5cfe17d9fbd8736d
PrastowoAdi <sinaadi75@gmail.com>
2022-03-13 13:52:40 +0700

    Create condition for navbar after user success login

--- components/organism/Navbar/Auth.tsx
+++ components/organism/Navbar/Auth.tsx
@@ -1,11 +1,26 @@
+import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-interface AuthProps {
-    isLogin ?: boolean;
-}
+export default function Auth() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState({
+    avatar: "",
+  });
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      const jwtToken = atob(token);
+      const payload = jwtDecode(jwtToken);
+      const users = payload.player;
+      const IMG = process.env.NEXT_PUBLIC_IMG;
+      user.avatar = `${IMG}/${user.avatar}`;
+      setIsLogin(true);
 
-export default function Auth(props: Partial<AuthProps>) {
-  const { isLogin } = props;
+      setUser(users);
+    }
+  }, []);
 
   if (isLogin) {
     return (
@@ -21,7 +36,7 @@ export default function Auth(props: Partial<AuthProps>) {
             aria-expanded="false"
           >
             <img
-              src="/img/avatar-1.png"
+              src={user.avatar}
               className="rounded-circle"
               width="40"
               height="40"
