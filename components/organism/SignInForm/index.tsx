e44f137d0eddf306d83692078c07ffafa81dd495
PrastowoAdi <sinaadi75@gmail.com>
2022-03-13 10:03:57 +0700

    Integration signin with API

--- components/organism/SignInForm/index.tsx
+++ components/organism/SignInForm/index.tsx
@@ -1,29 +1,58 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { setLogin } from "../../../services/auth";
 
 export default function SignInForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const onSumbit = async () => {
+    const data = {
+      email,
+      password,
+    };
+    if (!email || !password) {
+      toast.error("Email atau Password wajib diisi");
+    } else {
+      const response = await setLogin(data);
+      if (response.error) {
+        toast.error(response.message);
+      } else {
+        router.push("/");
+      }
+    }
+  };
   return (
     <>
       <h2 className="text-4xl fw-bold color-palette-1 mb-10">Sign In</h2>
       <p className="text-lg color-palette-1 m-0">Masuk untuk melakukan proses top up</p>
       <div className="pt-50">
-        <label htmlFor="email" className="form-label text-lg fw-medium color-palette-1 mb-10">
+        <label className="form-label text-lg fw-medium color-palette-1 mb-10">
           Email
           Address
         </label>
         <input
           type="email"
           className="form-control rounded-pill text-lg"
-          id="email"
-          name="email"
           aria-describedby="email"
           placeholder="Enter your email address"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </div>
       <div className="pt-30">
         <label
-          htmlFor="password"
           className="form-label text-lg fw-medium color-palette-1 mb-10"
         >
           Password
@@ -32,20 +61,20 @@ export default function SignInForm() {
         <input
           type="password"
           className="form-control rounded-pill text-lg"
-          id="password"
-          name="password"
           aria-describedby="password"
           placeholder="Your password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
       <div className="button-group d-flex flex-column mx-auto pt-50">
-        <a
+        <button
           className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16"
-          href="/"
-          role="button"
+          type="button"
+          onClick={onSumbit}
         >
           Continue to Sign In
-        </a>
+        </button>
         <Link href="/sign-up">
           <a
             className="btn btn-sign-up fw-medium text-lg color-palette-1 rounded-pill"
@@ -55,6 +84,7 @@ export default function SignInForm() {
             Up
           </a>
         </Link>
+        <ToastContainer />
       </div>
     </>
   );
