c858a70e0652c4c5e3bc8fc05d478548bee37825
PrastowoAdi <sinaadi75@gmail.com>
2022-04-20 17:28:33 +0700

    Integrasi sidebar profile

--- components/organism/Sidebar/Profile.tsx
+++ components/organism/Sidebar/Profile.tsx
@@ -1,10 +1,34 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 /* eslint-disable jsx-a11y/alt-text */
+
+import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
+import { useEffect, useState } from "react";
+import { JwtPayloadTypes, UserTypes } from "../../../services/data-types";
+
 export default function Profile() {
+  const [user, setUser] = useState({
+    avatar: "",
+    name: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JwtPayloadTypes = jwtDecode(jwtToken);
+      const userFromPlayload: UserTypes = payload.player;
+      setUser(userFromPlayload);
+    }
+  }, []);
+
   return (
     <div className="user text-center pb-50 pe-30">
-      <img src="/img/avatar-1.png" width="90" height="90" className="img-fluid mb-20" />
-      <h2 className="fw-bold text-xl color-palette-1 m-0">Shayna Anne</h2>
-      <p className="color-palette-2 m-0">shayna@anne.com</p>
+      <img src={user.avatar} width="90" height="90" className="img-fluid mb-20 profile-img" alt="profile" />
+      <h2 className="fw-bold text-xl color-palette-1 m-0">{user.name}</h2>
+      <p className="color-palette-2 m-0">{user.email}</p>
     </div>
   );
 }
