41d280c79661c88a6fe5473df8194e87a16ac5c2
PrastowoAdi <sinaadi75@gmail.com>
2022-05-12 22:06:17 +0700

    Integration table on transaction page

--- components/organism/TransactionContent/index.tsx
+++ components/organism/TransactionContent/index.tsx
@@ -1,16 +1,49 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
+import { useCallback, useEffect, useState } from "react";
+import NumberFormat from "react-number-format";
+import { toast } from "react-toastify";
+import { getMemberTransactions } from "../../../services/member";
 import ButtonTab from "./ButtonTab";
 import TableRow from "./TableRow";
 
 export default function TransactionContent() {
+  const [total, setTotal] = useState(0);
+  const [transactions, setTransactions] = useState([]);
+  const getMemberTransactionAPI = useCallback(async () => {
+    const response = await getMemberTransactions();
+    if (response.error) {
+      toast.error(response.message);
+    } else {
+      console.log("res", response);
+
+      setTotal(response.data.total);
+      setTransactions(response.data.history);
+    }
+  }, []);
+
+  useEffect(() => {
+    getMemberTransactionAPI();
+  }, []);
+
+  const IMG = process.env.NEXT_PUBLIC_IMG;
+
   return (
     <main className="main-wrapper">
       <div className="ps-lg-0">
         <h2 className="text-4xl fw-bold color-palette-1 mb-30">My Transactions</h2>
         <div className="mb-30">
           <p className="text-lg color-palette-2 mb-12">You’ve spent</p>
-          <h3 className="text-5xl fw-medium color-palette-1">Rp 4.518.000.500</h3>
+          <h3 className="text-5xl fw-medium color-palette-1">
+            <NumberFormat
+              value={total}
+              prefix="Rp. "
+              displayType="text"
+              thousandSeparator="."
+              decimalSeparator=","
+            />
+          </h3>
         </div>
         <div className="row mt-30 mb-20">
           <div className="col-lg-12 col-12 main-content">
@@ -36,10 +69,17 @@ export default function TransactionContent() {
                 </tr>
               </thead>
               <tbody id="list_status_item">
-                <TableRow image="overview-1" title="Mobile Legends:The New Battle 2021" category="Desktop" item={200} price={290000} status="Pending" />
-                <TableRow image="overview-2" title="Call of Duty:Modern" category="Desktop" item={550} price={740000} status="Success" />
-                <TableRow image="overview-3" title="Clash of Clans" category="Mobile" item={100} price={120000} status="Failed" />
-                <TableRow image="overview-4" title="The Royal Game" category="Mobile" item={225} price={200000} status="Pending" />
+                {transactions.map((transaction) => (
+                  <TableRow
+                    key={transaction._id}
+                    image={`${IMG}/${transaction.historyVoucherTopup.thumbnail}`}
+                    title={transaction.historyVoucherTopup.gameName}
+                    category={transaction.historyVoucherTopup.category}
+                    item={`${transaction.historyVoucherTopup.coinQuantity} ${transaction.historyVoucherTopup.coinName}`}
+                    price={transaction.value}
+                    status={transaction.status}
+                  />
+                ))}
               </tbody>
             </table>
           </div>
---
+++ services/member.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+
+import callAPI from "../config/api";
+
+const ROOT_API = process.env.NEXT_PUBLIC_API;
+const API_VERSION = "api/v1";
+
+export async function getMemberOverview() {
+  const url = `${ROOT_API}/${API_VERSION}/players/dashboard`;
+
+  return callAPI({
+    url,
+    method: "GET",
+    token: true,
+  });
+}
+
+export async function getMemberTransactions() {
+  const url = `${ROOT_API}/${API_VERSION}/players/history`;
+
+  return callAPI({
+    url,
+    method: "GET",
+    token: true,
+  });
+}
